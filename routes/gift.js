247e69fb5f198293ddaf6168d66f0cddbe2624c0
Jeffrey Kim <jeffreykim71@gmail.com>
2020-09-09 17:21:41 -0700

    Added Search Bar Function
    Added Search Gift Card Code Functionality

--- routes/gift.js
+++ routes/gift.js
@@ -30,7 +30,6 @@ router.post("/", ensureAuth, async (req, res) => {
 
 // @desc Show edit page
 // @route GET /stories/edit/id
-
 router.get("/edit/:id", ensureAuth, async (req, res) => {
   //Will look for a page called render within views/layouts
   const gift = await Gift.findOne({
@@ -45,6 +44,49 @@ router.get("/edit/:id", ensureAuth, async (req, res) => {
     res.render("giftcard/edit", {
       gift,
     });
+  }
+});
+
+//Search Story
+router.get("/search", ensureAuth, (req, res) => {
+  //Will look for a page called render within views/layouts
+  res.render("giftcard/search");
+});
+
+//Searching functionality works, it's just a matter of actually getting there
+router.get("/search/:id", ensureAuth, async (req, res) => {
+  const gift = await Gift.findOne({
+    giftID: req.params.id,
+  }).lean();
+  if (!gift) {
+    return res.render("error/404");
+  } else {
+    res.render("giftcard/show", {
+      gift,
+    });
+  }
+});
+
+router.get("", ensureAuth, (req, res) => {
+  res.render("dashboard");
+});
+
+//Show single story
+//Get stories/id
+router.get("/:id", ensureAuth, async (req, res) => {
+  //Will look for a page called render within views/layouts
+  try {
+    let gift = await Gift.findById(req.params.id).populate("user").lean();
+
+    if (!gift) {
+      return res.render("error/404");
+    }
+    res.render("giftcard/show", {
+      gift,
+    });
+  } catch (err) {
+    console.log(err);
+    res.render("error/404");
   }
 });
 
@@ -85,6 +127,18 @@ router.put("/:id", ensureAuth, async (req, res) => {
     });
   }
   res.redirect("/dashboard");
+});
+
+//Delete Story
+//DELETE /stories/:id
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    await Gift.remove({ _id: req.params.id });
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/error/500");
+  }
 });
 
 module.exports = router;
